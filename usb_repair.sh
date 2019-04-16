acb9465d416b8b677ec0fe404924bc9212cdaf6a
Charles Morin <charles.mor@outlook.fr>
2019-04-16 15:31:07 -0400

    Update usb_repair.sh

--- usb_repair.sh
+++ usb_repair.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 NUMBER_OF_KEYS=$(($(lsblk -do NAME,SIZE | grep -Ec '^([^l][^o][^o][^p]).*')-1))
+RE='^[0-9]+$'
 
-choose_usb() {
-        lsblk -do NAME,SIZE | grep -E '^([^l][^o][^o][^p]).*'
-        echo -e
+lsblk -do NAME,SIZE | grep -E '^([^l][^o][^o][^p]).*'
+echo -e
 
-        read -p "Choose a usb key to repair(#): " usb
-        if [ $usb -lt 1 ] || [ $usb -gt $NUMBER_OF_KEYS ]; then
-                #invalid usb number
-        fi
-
-        read -p "Are you sure you want to repair usb key $usb? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
-        if [ $choice != "y" ] || [ $choice != "n" ]; then
-                #invalid choice
-        fi
-}
-
-choose_usb
-while [ $? -ne 0 ]
+read -p "Choose a usb key to repair(#): " usb
+while ! [[ $usb =~ $RE ]] || [ $usb -lt 1 ] || [ $usb -gt $NUMBER_OF_KEYS ]
 do
-    echo -e "Error: choose a valid usb!"
-    choose_usb
+    echo "Error: enter a valid usb number(1-$NUMBER_OF_KEYS)"
+    read -p "Choose a usb key to repair(#): " usb
+done
+
+read -p "Are you sure you want to repair usb key $usb? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
+while [ "$choice" != "y" ] || [ "$choice" != "n" ]
+do
+    echo "Error: enter a valid choice(y/n)"
+    read -p "Are you sure you want to repair usb key $usb? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
 done
