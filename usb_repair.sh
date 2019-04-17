c0af8ab8723c353afdd0577f06e81a7908adbe2e
Charles Morin <charles.mor@outlook.fr>
2019-04-16 22:42:30 -0400

    Update usb_repair.sh

--- usb_repair.sh
+++ usb_repair.sh
@@ -7,18 +7,19 @@ echo "NAME         SIZE"
 lsblk -dno NAME,SIZE | grep -E '^([^l][^o][^o][^p]).*' | grep -En '*'
 echo -e
 
-read -p "Choose a usb key to repair(#): " usb
+read -n 1 -p "Choose a usb key to repair(#): " usb
 while ! [[ $usb =~ $RE ]] || [ $usb -lt 1 ] || [ $usb -gt $NUMBER_OF_KEYS ]
 do
-    echo "Error: enter a valid usb number(1-$NUMBER_OF_KEYS)"
-    read -p "Choose a usb key to repair(#): " usb
+    echo -e $'\nError: enter a valid usb number(1-'$NUMBER_OF_KEYS')'
+    read -n 1 -p "Choose a usb key to repair(#): " usb
 done
 
-read -n 1 -p "Are you sure you want to repair usb key $(lsblk -dno NAME | grep -E '^([^l][^o][^o]).*' | sed -ne "$usb"p)? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
+echo -e
+read -p "Are you sure you want to repair usb key $(lsblk -dno NAME | grep -E '^([^l][^o][^o]).*' | sed -ne "$usb"p)? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
 while [ "$choice" != "y" ] && [ "$choice" != "n" ]
 do
-    echo $'\nError: enter a valid choice(y/n)'
-    read -n 1 -p "Are you sure you want to repair usb key $(lsblk -dno NAME | grep -E '^([^l][^o][^o]).*' | sed -ne "$usb"p)? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
+    echo -e "Error: enter a valid choice(y/n)"
+    read -p "Are you sure you want to repair usb key $(lsblk -dno NAME | grep -E '^([^l][^o][^o]).*' | sed -ne "$usb"p)? `echo $'\n>' `It will delete all content on the usb (y/n): " choice
 done
 
 if [ "$choice" == "n" ]; then
@@ -27,6 +28,6 @@ fi
 
 disk="$(lsblk -dno NAME | grep -E '^([^l][^o][^o]).*' | sed -ne "$usb"p)"
 
-sudo fsck /dev/"$disk"
+sudo fsck -f /dev/"$disk"
 #sudo dd if=/dev/zero of=/dev/"$disk"
 #sudo mkfs.msdos -f 32 /dev/"$disk"1
